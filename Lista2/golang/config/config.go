63408280b8c22e8b5e5cd21ec4bbf50dede20010
karolszymonczyk <43317648+karolszymonczyk@users.noreply.github.com>
2019-04-28 13:43:47 +0200

    Update config.go

--- Lista2/golang/config/config.go
+++ Lista2/golang/config/config.go
@@ -1,14 +1,5 @@
 package config
 
-//const CHAIRMAN_RANGE = 2000
-//const EMPLOYEE_DELAY  = 3000
-//const CLIENT_DELAY = 6000
-//const STORE_CAPACITY = 10
-//const TASKS_CAPACITY = 15
-//const EMPLOYEES_AMOUNT = 4
-//const CLIENTS_AMOUNT = 6
-//const CHAIRMANS_AMOUNT = 2
-
 const CHAIRMAN_RANGE = 50
 const EMPLOYEE_DELAY  = 300
 const CLIENT_DELAY = 600
